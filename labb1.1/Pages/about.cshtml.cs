fa0a3252762fe0c39473b311e3ecc88377923fde
Hoomam <hoaz1600@student.miun.se>
2025-01-28 05:06:59 +0100

    Add project files.

---
+++ labb1.1/Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace labb1._1.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
